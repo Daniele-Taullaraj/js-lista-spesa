802b6da93244dfea8520208e04c46233461c97b7
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-04-24 16:17:48 +0200

    M2

--- scripts.js
+++ scripts.js
@@ -2,5 +2,11 @@ let lista = ["pasta", "riso", "frutta", "verdure", "carne", "pesce"]
 // console.log(lista)
 
 for (let i = 0; i < lista.length; i++) {
-    console.log(lista[i])
+    console.log("for " + lista[i])
+}
+
+let z = 0;
+while (z < lista.length) {
+    console.log("while " + lista[z]);
+    z++;
 }
