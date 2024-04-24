2241ee0f8eec1773ffa751bdb38f33c17fde4992
Daniele-Taullaraj <danieletaullaraj@gmail.com>
2024-04-24 16:31:30 +0200

    Bonus

--- scripts.js
+++ scripts.js
@@ -5,11 +5,28 @@ for (let i = 0; i < lista.length; i++) {
     console.log("for " + lista[i])
 }
 
+
+// con inner.html
+
+// const ul = document.getElementById("listaspesa");
+
+// let z = 0;
+// while (z < lista.length) {
+//     ul.innerHTML += `<li>${lista[z]}</li>`;
+//     console.log("while " + lista[z]);
+//     z++;
+// }
+
+
+
+// con crete element
 const ul = document.getElementById("listaspesa");
 
 let z = 0;
 while (z < lista.length) {
-    ul.innerHTML += `<li>${lista[z]}</li>`;
+    let li = document.createElement('li')
+    li.innerHTML = lista[z];
+    ul.appendChild(li)
     console.log("while " + lista[z]);
     z++;
 }
